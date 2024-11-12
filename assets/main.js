c48558eb2c7f532592ffbcd8e90551180f48115d
GABRIEL GARCIA H. <gagarcia@panama-tec.com>
2024-11-11 23:50:16 -0500

    funcionalidad de detalle de libros al 100%

--- assets/main.js
+++ assets/main.js
@@ -1,12 +1,13 @@
 import { toggle_element, redirigir } from "./modules/funcionesGenerales/index.js";
 import { request } from "./modules/request/index.js";
+import { mostrar_detalles_libros } from "./modules/detallesGenerales/index.js";
 
 const boton_formulario = document.getElementById('btn_login_formulario');
 const contenedor_libro = document.getElementById('contenedor_libros');
 const google_login = document.getElementById('google_login');
 
 const close_login = document.getElementById('close_login');
-const close_detalle = document.getElementById('close_detalle');
+
 
 const open_login = document.getElementById('open_login');
 
@@ -17,56 +18,71 @@ const detalle_libro = document.getElementById('detalle_libro');
 const GOOGLE_BOOK_URL = 'http://localhost/PARCIALES/PARCIAL_4/assets/php/GoogleBooks/google_book.php';
 
 //DEFECTOS DEL API DE GOOGLE BOOKS.
-const cantidad_libros = 20;
+const cantidad_libros = 40;
 const libro_buscado = "harry";
 var libros_disponibles = await request(`${GOOGLE_BOOK_URL}?cantidad_libros=${cantidad_libros}&libro_buscado=${libro_buscado}`);
-
 
 var arreglo_libros = libros_disponibles.items;
 
 
 const libros = (imagen, titulo, id) => {
+    
     return `
-    <div id='book_${id}' class="libro_contenedor_general">
+        <div  class="libro_contenedor_general">
+            
+            <div class='libro' >   
+                
+                
+                <div class='libro_caratula_inferior'> 
+                
+                </div>
+
+                <div class="libro_paginas" >
+
+                </div>
+
+                <div id='book_${id}' class="libro_portada libro_imagen" onclick='toggle_detalle(this.id)' style="background-image: url('${imagen}');">
+
+                </div>
+            
+                
+            </div>
+
+            <div class='libro_titulo'>
+                ${titulo}
+            </div>
         
-        <div class='libro' >   
-            
-            
-            <div class='libro_caratula_inferior'> 
-            
-            </div>
 
-            <div class="libro_paginas" >
-
-            </div>
-
-            <div class="libro_portada libro_imagen" onclick='toggle_detalle(1)' style="background-image: url('${imagen}');">
-
-            </div>
-          
-            
         </div>
-
-        <div class='libro_titulo'>
-            ${titulo}
-        </div>
-       
-
-    </div>
     
     `;
 } 
-
 
 const toggle_login = () => {
     toggle_element(login);
 }
 
-const toggle_detalle = () => {  
+const toggle_detalle = (id) => {  
+
+    if (id){
+
+        detalle_libro.innerHTML = "";
+    
+        let my_book_id = id.replace("book_", "");
+        let book_details = arreglo_libros[my_book_id];
+        
+        let titulo_libro = book_details.volumeInfo.title;
+        let descripcion = book_details.volumeInfo.description;
+        let imagen = book_details.volumeInfo.imageLinks.thumbnail;
+        let autor = book_details.volumeInfo.authors[0];
+        let ano_publicacion = book_details.volumeInfo.publishedDate;
+
+        // console.log(imagen);
+    
+        detalle_libro.innerHTML = mostrar_detalles_libros(titulo_libro, descripcion,imagen, autor, ano_publicacion);
+    }
     toggle_element(detalle_libro);
 }
-
-
 
 boton_formulario.addEventListener("click", (event) => {
     alert("presionado")
@@ -76,17 +92,10 @@ google_login.addEventListener("click", () => {
     redirigir('http://localhost/PARCIALES/PARCIAL_4/assets/php/GoogleOAuth/login.php');
 })
 
-
 close_login.addEventListener("click", ()=>{
     
    toggle_login();
 
-})
-
-close_detalle.addEventListener("click", ()=>{
-   
-    toggle_detalle();
- 
 })
 
 open_login.addEventListener("click", ()=>{
@@ -94,9 +103,6 @@ open_login.addEventListener("click", ()=>{
     toggle_login();
 
 })
-
-
-
 
 
 window.toggle_detalle = toggle_detalle;
