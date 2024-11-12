5bb0c1449edc23414d5a44d315b220b534b4f297
GABRIEL GARCIA H. <gagarcia@panama-tec.com>
2024-11-11 20:05:44 -0500

    funcionando al 100%

--- assets/main.js
+++ assets/main.js
@@ -3,16 +3,19 @@ const contenedor_libro = document.getElementById('contenedor_libros');
 const google_login = document.getElementById('google_login');
 
 const close_login = document.getElementById('close_login');
+const close_detalle = document.getElementById('close_detalle');
+
 const open_login = document.getElementById('open_login');
 
 const formulario_login_card = document.getElementById('formulario_login_card');
-const login = document.getElementById('login')
+const login = document.getElementById('login');
+const detalle_libro = document.getElementById('detalle_libro');
 
 const cantidad_libros = 50;
 
 const libros = () => {
     return `
-        <div class='libro'>   
+        <div class='libro' >   
             
             <div class='libro_caratula_inferior'> 
             
@@ -22,7 +25,7 @@ const libros = () => {
 
             </div>
 
-            <div class="libro_portada libro_imagen" >
+            <div class="libro_portada libro_imagen" onclick='toggle_detalle(1)'>
 
             </div>
 
@@ -35,18 +38,27 @@ const validar_existencia_clase = (elemento, clase) => {
     return Array.from(elemento.classList).includes(clase);
 }
 
-const toogle_login = () => {
-    
-    if (validar_existencia_clase(login, "open")){
-        login.classList.add("close");
-        login.classList.remove('open');
+const toggle_element = (toToggle) => {
+    if (validar_existencia_clase(toToggle, "open")){
+        toToggle.classList.add("close");
+        toToggle.classList.remove('open');
 
     }else{
         
-        login.classList.add("open");
-        login.classList.remove('close');
+        toToggle.classList.add("open");
+        toToggle.classList.remove('close');
     }
 }
+
+const toggle_login = () => {
+    
+    toggle_element(login);
+}
+
+const toggle_detalle = () => {  
+    toggle_element(detalle_libro);
+}
+
 
 boton_formulario.addEventListener("click", (event) => {
     alert("presionado")
@@ -58,13 +70,19 @@ google_login.addEventListener("click", () => {
 
 close_login.addEventListener("click", ()=>{
     
-   toogle_login();
+   toggle_login();
 
 })
 
+close_detalle.addEventListener("click", ()=>{
+    
+    toggle_detalle();
+ 
+ })
+
 open_login.addEventListener("click", ()=>{
     
-    toogle_login();
+    toggle_login();
 
 })
 
